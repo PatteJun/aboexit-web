c277a2b395ed68893ccc9addf5638580d731d829
Christoph Paterok <christophpaterok@Laptopvhristoph.fritz.box>
2024-11-15 07:57:40 +0100

    update

---
+++ src/data/categories.ts
@@ -0,0 +1,20 @@
+export const categoryNames = {
+  telekommunikation: {
+    de: 'Telekommunikation',
+    en: 'Telecommunications',
+    fr: 'Télécommunications',
+    it: 'Telecomunicazioni'
+  },
+  streaming: {
+    de: 'Streaming',
+    en: 'Streaming',
+    fr: 'Streaming',
+    it: 'Streaming'
+  },
+  energie: {
+    de: 'Energie',
+    en: 'Energy',
+    fr: 'Énergie',
+    it: 'Energia'
+  }
+}; 
