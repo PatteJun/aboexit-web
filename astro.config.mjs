b254d96101b2a414bf958db70029df69ad0ea838
Christoph Paterok <christophpaterok@Laptopvhristoph.fritz.box>
2024-11-23 12:39:05 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify'; // Import the Netlify adapter
 
 export default defineConfig({
   site: 'https://www.aboexit.ch',
+  trailingSlash: 'always', // Enforces trailing slashes on all URLs
+  output: 'static', // Ensures Astro generates static files
+  adapter: netlify(), // Initialize the Netlify adapter as an object
 });
